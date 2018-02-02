f8cab2e5234f3347607b5622f085df31043b11c4
xschen <xs0040@gmail.com>
2018-02-02 14:00:47 +0800

    store pokemon images into a folder

--- scrapy_projects/library/pokemon.py
+++ scrapy_projects/library/pokemon.py
@@ -1,4 +1,6 @@
 from scrapy_projects.library.chrome_driver_utils import make_headless_chrome_driver
+import urllib.request
+import os
 
 class Pokemon(object):
 
@@ -15,7 +17,16 @@ class Pokemon(object):
         self.pokemon_links = dict()
         self.pokemon_img_links = dict()
 
-    def download(self):
+    def download(self, output_dir):
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        output_img_dir_path = output_dir + '/img'
+        output_txt_dir_path = output_dir + '/txt'
+        if not os.path.exists(output_img_dir_path):
+            os.makedirs(output_img_dir_path)
+        if not os.path.exists(output_txt_dir_path):
+            os.makedirs(output_txt_dir_path)
+
         driver = make_headless_chrome_driver(self.chrome_driver_dir_path)
 
         for url in self.urls:
@@ -27,7 +38,7 @@ class Pokemon(object):
                 if len(img_div_elems) > 0 and len(h3_elems) > 0:
                     img_div_elem = img_div_elems[0]
                     h3_text = h3_elems[0].text
-                    pokemon_name = h3_text.split('.')[1].trim()
+                    pokemon_name = h3_text.split('.')[1].strip().lower()
                     print('found pokemon: ', pokemon_name)
                     img_elems = img_div_elem.find_elements_by_tag_name('img')
                     if len(img_elems) > 0:
@@ -36,8 +47,10 @@ class Pokemon(object):
                         img_src = img_elems[0].get_attribute("src")
                         self.pokemon_img_links[pokemon_name] = img_src
 
+                        urllib.request.urlretrieve(img_src, output_img_dir_path + '/' + pokemon_name + '.png')
+
         driver.close()
 
 
 if __name__ == '__main__':
-    Pokemon().download()
+    Pokemon().download(output_dir='../demo/temp/pokemon')
